bc306dc051d0b0b94e4cc5b4634312daea7488b5
Dylan Awalt-Conley <web@dylan.ac>
2026-04-23 14:13:47 -0400

    chore: delete unused code

--- addon/preferences.js
+++ addon/preferences.js
@@ -3,17 +3,5 @@
 window.__addonInstance___Preferences = {
   init: function () {
     Zotero.debug('__addonName__: Initialize preference pane');
-
-    const contrast = document.getElementById('__addonRef___default-contrast');
-    // checkbox.addEventListener('click', (e) => {
-    //   Zotero.debug(
-    //     'Focused Mode: toggled annotation pref: ' + checkbox.checked,
-    //   );
-    //   Zotero.Reader._readers.forEach((reader) => {
-    //     const doc = reader?._iframeWindow?.document?.documentElement;
-    //     if (!doc) return;
-    //     doc.dataset.hideAnnotationBar = checkbox.checked;
-    //   });
-    // });
   },
 };
